d952c87bf4a077a96f74e66041d6f35690a850b7
DismissedGuy <32306794+DismissedGuy@users.noreply.github.com>
2018-01-03 14:33:12 +0100

    Update bot.py
    added very simple bash command.
    TODO: Add command validation

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from geopy.distance import vincenty
+import subprocess
 from discord.ext.commands import Bot
 import discord
 
@@ -9,12 +10,18 @@ logging.basicConfig(level=logging.INFO) #set up logging to Heroku terminal
 client = Bot(description="A Dismissed Bot", command_prefix="::", pm_help = True)
 
 @client.command()
+async def bash(command):
+    x = subprocess.check_output([command])
+    
+    await client.say("Input: " + command + "\n Output: " + x)
+
+@client.command()
 async def dist(place1x, place1y, place2x, place2y):
     place1 = (place1x, place1y)
     place2 = (place2x, place1y)
     distance = vincenty(place1, place2).miles
 
-    await client.say(str(distance))
+    await client.say(str(distance) + " miles")
 
 @client.event
 async def on_ready():
