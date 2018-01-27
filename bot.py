6d007c237949c688cf725a729ae84c68fd4d2d58
DismissedGuy <almeloomike@gmail.com>
2018-01-27 17:30:03 +0100

    big updates come with many bug fixes

--- bot.py
+++ bot.py
@@ -11,21 +11,21 @@ startup_extensions = ["cogs." + ext for ext in os.listdir("cogs/")]
 owner = '311869975579066371'
 client = Bot(description="A Dismissed Bot", command_prefix="::", pm_help = True)
 
-@bot.command()
+@client.command()
 async def load(extension_name : str):
     """Loads an extension."""
     try:
-        bot.load_extension(extension_name)
+        client.load_extension(extension_name)
     except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        await client.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
         return
-    await bot.say("{} loaded.".format(extension_name))
+    await client.say("{} loaded.".format(extension_name))
 
-@bot.command()
+@client.command()
 async def unload(extension_name : str):
     """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
+    client.unload_extension(extension_name)
+    await client.say("{} unloaded.".format(extension_name))
 
 @client.event
 async def on_ready():
@@ -38,9 +38,9 @@ async def on_ready():
 if __name__ == "__main__": #load cogs
     for extension in startup_extensions:
         try:
-            bot.load_extension(extension)
+            client.load_extension(extension)
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n{}'.format(extension, exc))
 
-    bot.run(os.environ['BOT_TOKEN'])
+    client.run(os.environ['BOT_TOKEN'])
