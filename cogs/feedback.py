9de269de0563cacf36d270eea2e2a66806d83753
DismissedGuy <almeloomike@gmail.com>
2018-02-02 14:57:01 +0100

    since when doesn't this work

--- cogs/feedback.py
+++ cogs/feedback.py
@@ -10,12 +10,12 @@ class Feedback():
 			print("go return")
 			return
 		
-		await self.client.say("You want to send this feedback to the owner:\n```{}```\nIs this correct? (Yes/No)".format(message.content))
+		await self.client.send_message(message.author, "You want to send this feedback to the owner:\n```{}```\nIs this correct? (Yes/No)".format(message.content))
 		
 		confirm = await self.client.wait_for_message(timeout=60.0, channel=message.author)
 		
 		if confirm == None or not confirm.content.lower() in ["yes", "y", "yea", "yep", "ye"]:
-			await self.client.say(":x: Feedback discarded.")
+			await self.client.send_message(message.author, ":x: Feedback discarded.")
 			return
 		
 		embed = discord.Embed(color=0xFF000)
@@ -23,7 +23,7 @@ class Feedback():
 		embed.set_footer(text=confirm.timestamp)
 		
 		await self.client.send_message(self.client.get_channel("408972669615341568"), ":exclamation: Received a new feedback!", embed=embed)
-		await self.client.say(":white_check_mark: Successfully sent feedback!")
+		await self.client.send_message(message.author, ":white_check_mark: Successfully sent feedback!")
 		
 def setup(bot):
 	bot.add_cog(Feedback(bot))
