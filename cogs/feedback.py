bbcbe144189ccc14a9827b12a48ee5ad1f148bbc
DismissedGuy <almeloomike@gmail.com>
2018-02-02 15:09:55 +0100

    try again

--- cogs/feedback.py
+++ cogs/feedback.py
@@ -12,7 +12,7 @@ class Feedback():
 		
 		await self.client.send_message(message.author, "You want to send this feedback to the owner:\n```{}```\nIs this correct? (Yes/No)".format(message.content))
 		
-		confirm = await self.client.wait_for_message(timeout=60.0, channel=message.author)
+		confirm = await self.client.wait_for_message(timeout=60.0, author=message.author, channel=None)
 		
 		if confirm == None or not confirm.content.lower() in ["yes", "y", "yea", "yep", "ye"]:
 			await self.client.send_message(message.author, ":x: Feedback discarded.")
