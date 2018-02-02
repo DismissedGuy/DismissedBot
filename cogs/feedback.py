44f113e7936c0cdd0f3ab302ed09f6c60f33d1da
DismissedGuy <almeloomike@gmail.com>
2018-02-02 15:25:17 +0100

    fuck reference errors

--- cogs/feedback.py
+++ cogs/feedback.py
@@ -4,7 +4,7 @@ import discord
 class Feedback():
 	def __init__(self, bot):
 		self.client = bot
-		sending = False
+		self.sending = False
 		
 	async def on_message(self, message):
 		if message.author == self.client.user or not message.channel.name == None or message.content.startswith('::'):
@@ -15,7 +15,7 @@ class Feedback():
 		
 		await self.client.send_message(message.author, "You want to send this feedback to the owner:\n```{}```\nIs this correct? (Yes/No)".format(message.content))
 		
-		sending=True
+		self.sending=True
 		confirm = await self.client.wait_for_message(timeout=60.0, author=message.author, channel=None)
 		
 		if confirm == None or not confirm.content.lower() in ["yes", "y", "yea", "yep", "ye"]:
@@ -28,7 +28,7 @@ class Feedback():
 		
 		await self.client.send_message(self.client.get_channel("408972669615341568"), ":exclamation: Received a new feedback!", embed=embed)
 		await self.client.send_message(message.author, ":white_check_mark: Successfully sent feedback!")
-		sending=False
+		self.sending=False
 		
 def setup(bot):
 	bot.add_cog(Feedback(bot))
