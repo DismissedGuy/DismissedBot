9e0c579e482defebac65172113463296a60ecc58
DismissedGuy <almeloomike@gmail.com>
2018-02-13 16:57:55 +0100

    added eval, removed presence

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,7 +1,9 @@
 import discord
 import inspect
-import difflib
+import io
 import subprocess
+import traceback
+from contextlib import redirect_stdout
 from discord.ext import commands
 
 class Owner():
@@ -48,6 +50,52 @@ class Owner():
         
         await ctx.send(python.format(result))
 
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    @commands.is_owner()
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
     @commands.command(hidden=True)
     @commands.is_owner()
     async def announcement(self, ctx, *, announcement=None):
@@ -61,16 +109,6 @@ class Owner():
             except:
                 pass
             await msgchannel.send(embed=embed)
-
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def presence(self, ctx, *, game=None):
-        await self.client.change_presence(game=discord.Game(name=game, type=0))
-        
-        if game == None:
-            await ctx.send('Removed the current playing status!')
-        else:
-            await ctx.send('Changed playing status to "{}"!'.format(game))
 
 def setup(bot):
     bot.add_cog(Owner(bot))
