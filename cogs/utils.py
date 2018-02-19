5000549b80c17de1da7b4e417793913c0876c55f
DismissedGuy <almeloomike@gmail.com>
2018-02-19 18:41:41 +0100

    hope this works.. major command structure change

--- cogs/utils.py
+++ cogs/utils.py
@@ -8,60 +8,23 @@ class Utilities():
 
     def __init__(self, bot):
         self.client = bot
-        
-    async def get_member_by_name(self, ctx, search):
-        names = difflib.get_close_matches(search, [z.name for z in ctx.guild.members], n=5, cutoff=0.5)
-        users = []
-        for i in names:
-            users.append(discord.utils.find(lambda z: z.name == i, ctx.guild.members))
-        
-        if not users:
-            await ctx.send(":x: I couldn't find any users! Tip: try an ID as input.")
-            return False #error occured
-        elif len(users) > 1:
-            await ctx.send(":exclamation: Multiple users were found:\n- " + '\n- '.join(["**"+str(v)+"**" for v in users]))
-            return False #error occured
-        else: #1 user found, return the ID of that user
-            return users[0].id
-        
+
     @commands.command(description='Fetch the avatar of a user')
-    async def avatar(self, ctx, id):
-        if not id.isdecimal(): #assume they're passing a search
-            id = await self.get_member_by_name(ctx, id)
-            if not id: #returned an error
-                return #handling is already done
-        
-        try:
-            user = await self.client.get_user_info(id)
-        except:
-            await ctx.send(":x: An error occured while fetching the user! Please check your input and try again.")
-            return
-            
+    async def avatar(self, ctx, user : discord.User):
         embed = discord.Embed(title=user.avatar_url, color=16648720)
         embed.set_image(url=user.avatar_url)
-        
-        await ctx.send(":white_check_mark: Here's the avatar for `{0.name}#{0.discriminator}`!".format(user), embed=embed)
+
+        await ctx.send(":white_check_mark: Here's the avatar for `{}`!".format(str(user)), embed=embed)
 
     @commands.command(description='Check dbans for a user ID')
-    async def dbans(self, ctx, id):
-        if not id.isdecimal(): #assume they're passing a search
-            id = await self.get_member_by_name(ctx, id)
-            if not id: #returned an error
-                return #handling is already done
-        
-        try:
-            user = await self.client.get_user_info(int(id))
-        except:
-            await ctx.send(':x: Please enter a correct user ID!')
-            return
-        
+    async def dbans(self, ctx, user : discord.User):
         payload = {
             'token': os.environ['DBANS_TOKEN'],
-            'userid': id,
+            'userid': user.id,
             'version': 3,
         }
         r = requests.post('https://bans.discordlist.net/api', data=payload)
-        
+
         #very dirty I know
         if r.text == 'False':
             'not in DBans'
@@ -74,25 +37,18 @@ class Utilities():
             reason = r.json()[3]
             proof = r.json()[4][9:(- 11)]
             color = 16648720
-        
+
         embed = discord.Embed(color=color)
         embed.set_thumbnail(url=user.avatar_url)
-        embed.add_field(name='User:', value='{0.name}#{0.discriminator} (ID: {0.id})'.format(user), inline=False)
+        embed.add_field(name='User:', value='{0} (ID: {0.id})'.format(str(user), user), inline=False)
         embed.add_field(name='Is on DBans:', value=listed, inline=False)
 
         if listed:
             embed.add_field(name='Report ID:', value=rid, inline=True)
             embed.add_field(name='Reason:', value=reason, inline=True)
             embed.add_field(name='Proof:', value=proof, inline=True)
-        
-        await ctx.send(':white_check_mark: DBans list fetched!', embed=embed)
 
-    @commands.command()
-    async def dist(self, ctx, long1, lat1, long2, lat2):
-        place1 = (long1, lat2)
-        place2 = (long2, lat2)
-        distance = vincenty(place1, place2).miles
-        await ctx.send(str(distance) + ' miles')
+        await ctx.send(':white_check_mark: DBans list fetched!', embed=embed)
 
     @commands.command(description='Shows the current time.')
     async def time(self, ctx, *, tz='Europe/London'):
@@ -102,24 +58,24 @@ class Utilities():
             await ctx.channel.send(file=discord.File('tz.txt', filename='tz.txt'))
             os.remove('tz.txt')
             return
-        
+
         if (not (tz in pytz.all_timezones)):
             await ctx.send(':x: Invalid timezone specified! For a list of all timezones, do `::time list`.')
             return
-        
+
         currmsg = await ctx.send("Please wait while I'm getting the time...")
         confirm = 'Time for `{}`:'.format(tz)
-        
+
         for i in range(21):
             await asyncio.sleep(3)
             now = datetime.datetime.now(pytz.timezone(tz))
             date = ':calendar: {0.day}-{0.month}-{0.year}'.format(now)
             time = ':clock: {0.hour}:{0.minute}:{0.second}'.format(now)
-            
+
             embed = discord.Embed(color=16711680)
             embed.add_field(name=date, value=time, inline=False)
             embed.set_footer(text='All info provided by my system time.')
-            
+
             await currmsg.edit(new_content=confirm, embed=embed, content=confirm)
 
 def setup(bot):
